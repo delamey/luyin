b3ee6880582c51aada3c14ab944cdf458f948c9a
WHR <ssssss>
2018-06-12 16:47:29 +0800

    11

---
+++ app/src/main/java/com/example/administrator/myapplication/AppConfig.java
@@ -0,0 +1,10 @@
+package com.example.administrator.myapplication;
+
+/**
+ * Created by Administrator on 2017/9/23.
+ */
+
+public class AppConfig {
+    public static String IPAddress = "192.168.1.115";// 局域网
+    public static int Port = 30000;
+}
